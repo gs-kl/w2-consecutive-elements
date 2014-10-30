bf9d752088512e639cc775eac80e3d86d7d85f54
Kevin <kevin@Kevins-MacBook-Pro.local>
2014-10-30 10:48:02 -0700

    Add testing

--- script.rb
+++ script.rb
@@ -1,26 +1,28 @@
-# ary = ["archery", "archery", "swimming", "swimming", "archery", "archery", "crafts"] # should return true
-ary = ["archery", "swimming", "swimming", "swimming", "crafts", "archery", "archery", "crafts"] # should return false
-
-ary2 = []
-track = []
-ary.each do |a|
-	if a != track[-1]	
-		ary2.push([a, 1])
-	else
-		ary2[-1][1] = ary2[-1][1].succ
+class Schedulestuff
+	def self.formatarray ary
+		ary2 = []
+		track = []
+		ary.each do |a|
+			if a != track[-1]	
+				ary2.push([a, 1])
+			else
+				ary2[-1][1] = ary2[-1][1].succ
+			end
+			track.push(a)
+		end
+		return ary2
 	end
-	track.push(a)
-end
-print "Formatted array: " + ary2.to_s + "\n"
 
-result = true
-track2 = [2]
-ary2[0..-2].each do |a|
-	if a[1] != track2[-1]
-		result = false
+	def self.evaluatecompliance ary 
+		ary2 = formatarray ary
+		result = true
+		track2 = [2]
+		ary2[0..-2].each do |a|
+			if a[1] != track2[-1]
+				result = false
+			end
+			track2.push(a[1])
+		end
+		result
 	end
-	track2.push(a[1])
 end
-
-print "Result: " + result.to_s
-
