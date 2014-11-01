f17acb602abb5377172e5848c9915c4ce0d2b16e
Kevin <kevin@Kevins-MacBook-Pro.local>
2014-10-31 17:40:03 -0700

    Fix tabs

--- script.rb
+++ script.rb
@@ -1,28 +1,28 @@
 class Schedulestuff
-	def self.formatarray ary
-		ary2 = []
-		track = []
-		ary.each do |a|
-			if a != track[-1]	
-				ary2.push([a, 1])
-			else
-				ary2[-1][1] = ary2[-1][1].succ
-			end
-			track.push(a)
-		end
-		return ary2
-	end
+  def self.formatarray ary
+    ary2 = []
+    track = []
+    ary.each do |a|
+      if a != track[-1]  
+        ary2.push([a, 1])
+      else
+        ary2[-1][1] = ary2[-1][1].succ
+      end
+      track.push(a)
+    end
+    return ary2
+  end
 
-	def self.evaluatecompliance ary 
-		ary2 = formatarray ary
-		result = true
-		track2 = [2]
-		ary2[0..-2].each do |a|
-			if a[1] != track2[-1]
-				result = false
-			end
-			track2.push(a[1])
-		end
-		result
-	end
+  def self.evaluatecompliance ary 
+    ary2 = formatarray ary
+    result = true
+    track2 = [2]
+    ary2[0..-2].each do |a|
+      if a[1] != track2[-1]
+        result = false
+      end
+      track2.push(a[1])
+    end
+    result
+  end
 end
--- script_test.rb
+++ script_test.rb
@@ -2,11 +2,11 @@ require 'minitest/autorun'
 require_relative 'script'
 
 class SchedulestuffTest < MiniTest::Unit::TestCase
-	def test_compliant
-		assert_equal true, Schedulestuff.evaluatecompliance(["archery", "archery", "swimming", "swimming", "archery", "archery", "crafts"])
-	end
+  def test_compliant
+    assert_equal true, Schedulestuff.evaluatecompliance(["archery", "archery", "swimming", "swimming", "archery", "archery", "crafts"])
+  end
 
-	def test_noncompliant
-		assert_equal false, Schedulestuff.evaluatecompliance(["archery", "swimming", "swimming", "swimming", "crafts", "archery", "archery", "crafts"])
-	end
+  def test_noncompliant
+    assert_equal false, Schedulestuff.evaluatecompliance(["archery", "swimming", "swimming", "swimming", "crafts", "archery", "archery", "crafts"])
+  end
 end
